e3799327c3629e57c3d2f048f224cd285e325bb0
Dario <dariopresdev@protonmail.com>
2023-02-13 14:11:21 +0100

    float term keymap

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,9 +1,19 @@
-vim.keymap.set("n",  "<leader>q" ,function() 
+vim.keymap.set("n",  "<leader>q", function() 
     vim.cmd(':FloatermNew');
 end)
-vim.keymap.set("n",  "<leader>qg" ,function() 
+vim.keymap.set("n",  "<leader>qg", function() 
    vim.cmd(':FloatermNew! --height=0.9 --width=0.9 lazygit ');
 end)
+vim.keymap.set("n",  "<leader>qx", function() 
+    vim.cmd(':FloatermHide');
+end)
+vim.keymap.set("n",  "<leader>qs", function() 
+    vim.cmd(':FloatermShow');
+end)
+vim.keymap.set("n",  "<leader>qn" ,function() 
+    vim.cmd(':FloatermNext');
+end)
+
 
 --vim.api.nvim_set_keymap("n",  "<leader>q" , <C-\><C-n>:FloatermNew<CR>
 --vim.api.nvim_set_keymap("n",  <F8>    :FloatermPrev<CR>

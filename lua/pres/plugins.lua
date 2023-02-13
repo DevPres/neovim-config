e1e6d726cc7388e70948036d4b46f291a06020e7
Dario <dariopresdev@protonmail.com>
2023-02-13 02:52:41 +0100

    removed nightly tag fro icons

--- lua/pres/plugins.lua
+++ lua/pres/plugins.lua
@@ -63,7 +63,6 @@ return require('packer').startup(function(use)
         requires = {
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
         },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
     }
 end)
 

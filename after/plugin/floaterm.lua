0e8eace4d1fba837466fb191a836a67ee61fd6c6
Dario <dariopresdev@protonmail.com>
2023-02-13 12:44:07 +0100

    key-binding for lazygit

---
+++ after/plugin/floaterm.lua
@@ -0,0 +1,14 @@
+vim.keymap.set("n",  "<leader>q" ,function() 
+    vim.cmd(':FloatermNew');
+end)
+vim.keymap.set("n",  "<leader>qg" ,function() 
+   vim.cmd(':FloatermNew! --height=0.9 --width=0.9 lazygit ');
+end)
+
+--vim.api.nvim_set_keymap("n",  "<leader>q" , <C-\><C-n>:FloatermNew<CR>
+--vim.api.nvim_set_keymap("n",  <F8>    :FloatermPrev<CR>
+--vim.api.nvim_set_keymap("n",  <F8>    <C-\><C-n>:FloatermPrev<CR>
+--vim.api.nvim_set_keymap("n",  <F9>    :FloatermNext<CR>
+--vim.api.nvim_set_keymap("n",  <F9>    <C-\><C-n>::FloatermNext<CR>
+--vim.api.nvim_set_keymap("n",  <F12>   :FloatermToggle<CR>
+--vim.api.nvim_set_keymap("n",  <F12>   <C-\><C-n>:FloatermToggle<CR>

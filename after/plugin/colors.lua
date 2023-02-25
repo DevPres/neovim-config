f708b52ef00618328e80e9362771f381667bd5bb
Dario <dariopresdev@protonmail.com>
2023-02-26 00:28:41 +0100

    fixed colors nvmi tree

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -13,10 +13,10 @@ require("tokyonight").setup({
     functions = {},
     variables = {},
     -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "normal", -- style for sidebars, see below
+    sidebars = "transparent", -- style for sidebars, see below
     floats = "transparent", -- style for floating windows
   },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  --sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
   dim_inactive = false, -- dims inactive windows
@@ -31,7 +31,7 @@ require("tokyonight").setup({
   --- function will be called with a Highlights and ColorScheme table
   on_highlights = function(highlights, colors)
       highlights.VertSplit = {
-          fg = colors.none 
+          fg = colors.none, 
       }
   end,
 })
@@ -45,6 +45,9 @@ function ColorThings(color)
     -- For Transparency
     vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg="none"})
+    
 end
+
 
 ColorThings()

947b3fb3e60d272600d67764c709e9105ba69c46
Dario <dariopresdev@protonmail.com>
2023-02-26 00:28:51 +0100

    resolved lsp situation

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,72 +1,117 @@
-local lsp = require("lsp-zero")
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
 
-lsp.preset("recommended")
-
-lsp.ensure_installed({
-    "tailwindcss",
-    'tsserver',
-    'rust_analyzer',
+})
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "lua_ls",
+        "tailwindcss",
+        'tsserver',
+        'rust_analyzer',
+    }
 })
 
+local on_attach = function (_,_)
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gs', require('telescope.builtin').lsp_references, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+end
 
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
+local lspconfig = require('lspconfig')
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
-require('lspconfig').tsserver.setup({
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+lspconfig.tsserver.setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
     init_options = {
         preferences = {
             disableSuggestions = true
         }
     }
-})
-
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
+}
+require'lspconfig'.lua.setup {
+    settings = {
+        -- Settings go here!
     }
-})
+}
+-- luasnip setup
+local luasnip = require 'luasnip'
 
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "hi", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
 
-lsp.setup()
 
-require('lspconfig').tsserver.setup({
-    init_options = {
-        preferences = {
-            disableSuggestions = true
-        }
-    }
-})
+
+
+--lsp.on_attach(function(client, bufnr)
+--  local opts = {buffer = bufnr, remap = false}
+--
+--  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+--  vim.keymap.set("n", "hi", function() vim.lsp.buf.hover() end, opts)
+--  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+--  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+--  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+--  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+--  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+--  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+--  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+--  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+--end)
+--
+--lsp.setup()
 
 vim.diagnostic.config({
     virtual_text = true,
@@ -84,12 +129,5 @@ vim.diagnostic.config({
     },
  })
 
-require('mason').setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
-})
+
+
